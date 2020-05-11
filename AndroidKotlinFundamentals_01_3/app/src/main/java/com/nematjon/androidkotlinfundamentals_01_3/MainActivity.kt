a919bc71a85aabed14e3d9342f29a025c650b76a
nnarziev95 <nnarziev@gmail.com>
2020-05-11 20:00:32 +0900

    Coding challenge for two dices is completed

--- AndroidKotlinFundamentals_01_3/app/src/main/java/com/nematjon/androidkotlinfundamentals_01_3/MainActivity.kt
+++ AndroidKotlinFundamentals_01_3/app/src/main/java/com/nematjon/androidkotlinfundamentals_01_3/MainActivity.kt
@@ -9,18 +9,25 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage: ImageView
+    lateinit var diceImage1: ImageView
+    lateinit var diceImage2: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        diceImage = findViewById(R.id.dice_iamge)
+        diceImage1 = findViewById(R.id.dice_iamge1)
+        diceImage2 = findViewById(R.id.dice_iamge2)
         val rollDice: Button = findViewById(R.id.roll_button)
         rollDice.setOnClickListener { rollDice() }
     }
 
-    private fun rollDice(){
+    private fun rollDice() {
+        diceImage1.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage(): Int {
         val randomInt = (1..6).random()
-        val drawableResource = when (randomInt){
+        val drawableId = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -28,6 +35,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
+        return drawableId
     }
 }
